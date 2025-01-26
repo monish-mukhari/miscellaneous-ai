dfb320a77433bd621fb9ec03a5a7125cfc0c1835
monish-mukhari <manishmukherjee91@gmail.com>
2025-01-26 17:56:11 +0530

    setup llm and autoprompting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { db } from "./db";
+import { db } from "./db/index.js";
 import { todosTable } from "./db/schema";
 import { ilike, eq } from "drizzle-orm";
+import readlineSync from "readline-sync";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 async function getAllTodos() {
     const todos = await db.select().from(todosTable);
@@ -8,10 +13,14 @@ async function getAllTodos() {
 }
 
 async function createTodo(title: string, description: string) {
-    await db.insert(todosTable).values({
+    const [ result ] = await db.insert(todosTable).values({
         title,
         description,
+    }).returning({
+        id: todosTable.id
     });
+
+    return result.id;
 }
 
 async function searchTodo(search: string) {
@@ -21,4 +30,88 @@ async function searchTodo(search: string) {
 
 async function deleteTodoById(id: number) {
     await db.delete(todosTable).where(eq(todosTable.id, id));
+}
+
+
+const tools = {
+    getAllTodos: getAllTodos,
+    createTodo: createTodo,
+    deleteTodoById: deleteTodoById,
+    searchTodo: searchTodo,
+};
+
+const SYSTEM_PROMPT = `
+
+    You are an AI To-Do List Assistant with START, PLAN, ACTION, Observation and Output State.
+    Wait for the user prompt and first PLAN using available tools.
+    After planning, Take the action with appropriate tools and wait for Observation based on Action.
+    Once you get the observations, Return the AI response based on START prompt and observations
+
+    You can manage tasks by adding, viewing, updating, and deleting them.
+    You must strictly follow the JSON output format.
+
+    Todo DB Schema:
+    id: Int and Primary Key
+    title: String
+    description: String
+    created_at: Date Time
+    updated_at: Date Time
+
+    Available Tools:
+    - getAllTodos(): Returns all the Todos from Database
+    - createTodo(title: string, description: string): Creates a new Todo in the DB and takes todo as a string and returns the ID of created todo
+    - deleteTodoById(id: string): Deleted the todo by ID given in the DB
+    - searchTodo(query: string): Searches for all todos matching the query string using iLike in DB
+
+    Example:
+    START
+    { "type": "user", "user": "Add a task for shopping groceries." }
+    { "type": "plan", "plan": "I will try to get more context on what user needs to shop." }
+    { "type": "output", "output": "Can you tell me what all items you want to shop for?" }
+    { "type": "user", "user": "I want to shop for milk, kurkure, lays and choco." }
+    { "type": "plan", "plan": "I will use createTodo to create a new Todo in DB." }
+    { "type": "action", "function": "createTodo", "input": "Shopping for milk, kurkure, lays and choco." } 
+    { "type": "observation", "observation": "2" }
+    { "type": "output", "output": "Your todo has been added successfully" }
+
+`;
+
+const messages = [ { role: 'system', content: SYSTEM_PROMPT } ];
+
+
+while(true) {
+    const query = readlineSync.question(">> ");
+    const userMessage = {
+        type: "user",
+        user: query
+    };
+
+    messages.push({ role: "user", content: JSON.stringify(userMessage) });
+
+    // autoprompting
+
+    while(true) {
+        const chat = await model.generateContent(messages.toString())
+        const result = chat.response.text();
+
+        messages.push({ role: 'assistant', content: result });
+        const action = JSON.parse(result);
+
+        if(action.type === 'output') {
+            console.log(`🤖: ${action.output}`);
+            break;
+        } else if(action.type === 'action') {
+            // @ts-ignore
+            const fn = tools[action.function];
+            if(!fn) throw new Error('Invalid Tool call');
+            const observation = await fn(action.input);
+            const observationMessage = {
+                type: "observation",
+                observation: observation
+            }
+
+            messages.push({ role: 'developer', content: JSON.stringify(observationMessage) });
+        } 
+        
+    }
 }
